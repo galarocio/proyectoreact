52fbbe3be8e9b3b1f2e7c207424abce2f80c1eaa
gala sanchez <galitasanchez@hotmail.com>
2022-01-26 23:48:56 -0300

    newcomponents

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,33 +1,30 @@
 import "./NavBar.css";
+import CardWidget from "./CardWidget";
 let terceropcion = "Carrito";
 
 function NavBar() {
   return (
     <>
-      <ul className="nav justify-content-center">
-        <li className="nav-item">
-          <a className="nav-link" href="#">
-            Inicio
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">
-            Catalogo
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">
-            Carrito
-          </a>
-        </li>
-      </ul>
-      {/* <nav className="nav">
-        <ul>
-          <li>Inicio</li>
-          <li>Catalogo</li>
-          <li>{terceropcion}</li>
+      <nav>
+        <ul className="nav justify-content-center">
+          <CardWidget />
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Inicio
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Catalogo
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Carrito
+            </a>
+          </li>
         </ul>
-      </nav> */}
+      </nav>
     </>
   );
 }
